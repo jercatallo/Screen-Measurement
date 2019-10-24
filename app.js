f013fdf66bd4b1345ef033b55a627e217286ffac
Jer Carlo Catallo <49897728+rojerthat07@users.noreply.github.com>
2019-10-24 13:48:59 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+
+
+var screenWidth = document.getElementById("screen-width").innerHTML = screen.width;
+var screenHeight = document.getElementById("screen-height").innerHTML = screen.height;
+
+
+
+
+
+function getWidthHeight() {
+    document.getElementById("screen-width").innerHTML = screen.width;
+    document.getElementById("screen-height").innerHTML = screen.height;
+}
